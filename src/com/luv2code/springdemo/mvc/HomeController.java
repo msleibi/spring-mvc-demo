39d9a0b07a0d1f15ab89f95980d672218d8bbf38
Mohammad Sleibi <msalibi83@gmail.com>
2021-01-10 17:18:56 +0100

    Create the Mvc Project and adding: Controller - Html form

---
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		
+		return "main-menu";
+	}
+	
+	@RequestMapping("/showForm")
+	public String showingForm() {
+		
+		return "helloWorldForm";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processingForm() {
+		
+		return "showResult";
+	}
+	
+	
+}
